c862a244499ac83252854f751c4e5ce803935426
elimsc <zhjiang.rtc@gmail.com>
2022-02-17 04:06:09 +0800

    rdd_fast_stark

--- code/test_fast_stark.py
+++ code/test_fast_stark.py
@@ -5,6 +5,24 @@ from rescue_prime import *
 from fri import *
 from ip import *
 from fast_stark import *
+import sys
+
+from rdd_fast_stark import FastStark as RddFastStark
+
+from pyspark import SparkContext, SparkConf
+
+
+conf = (
+    SparkConf().setAppName("test_fast_stark").setMaster("spark://zhdeMacBook-Pro:7077")
+)
+sc = SparkContext(conf=conf)
+
+sc.addPyFile("./algebra.py")
+sc.addPyFile("./rdd_ntt.py")
+sc.addPyFile("./univariate.py")
+sc.addPyFile("./rdd_merkle.py")
+sc.addPyFile("./rdd_poly.py")
+# sys.path.insert(0, SparkFiles.getRootDirectory())
 
 
 def test_fast_stark():
@@ -16,7 +34,7 @@ def test_fast_stark():
     rp = RescuePrime()
     output_element = field.sample(bytes(b"0xdeadbeef"))
 
-    for trial in range(0, 20):
+    for trial in range(0, 1):  # 20
         input_element = output_element
         print("running trial with input:", input_element.value)
         output_element = rp.hash(input_element)
@@ -83,4 +101,60 @@ def test_fast_stark():
         print("proof rejected! \\o/")
 
 
-test_fast_stark()
+def test_rdd_fast_stark():
+    field = Field.main()
+    expansion_factor = 4
+    num_colinearity_checks = 2
+    security_level = 2
+
+    rp = RescuePrime()
+    output_element = field.sample(bytes(b"0xdeadbeef"))
+
+    for trial in range(0, 1):  # 20
+        input_element = output_element
+        print("running trial with input:", input_element.value)
+        output_element = rp.hash(input_element)
+        num_cycles = rp.N + 1
+        state_width = rp.m
+
+        stark = RddFastStark(
+            field,
+            expansion_factor,
+            num_colinearity_checks,
+            security_level,
+            state_width,
+            num_cycles,
+            sc=sc,
+        )
+        (
+            transition_zerofier,
+            transition_zerofier_codeword,
+            transition_zerofier_root,
+        ) = stark.preprocess()
+
+        # prove honestly
+        print("honest proof generation ...")
+
+        # prove
+        trace = rp.trace(input_element)
+        trace1 = []
+        for i in range(stark.num_registers):
+            trace1 += [sc.parallelize([(j, trace[j][i]) for j in range(len(trace))])]
+        assert len(trace1) == stark.num_registers, "len(trace1) == stark.num_registers"
+        assert trace1[0].take(1)[0][0] == 0
+        trace = trace1
+        air = rp.transition_constraints(stark.omicron)
+        boundary = rp.boundary_constraints(output_element)
+        proof = stark.prove(
+            trace, air, boundary, transition_zerofier, transition_zerofier_codeword
+        )
+
+        # verify
+        verdict = stark.verify(proof, air, boundary, transition_zerofier_root)
+
+        assert verdict == True, "valid stark proof fails to verify"
+        print("success \\o/")
+
+
+# test_fast_stark()
+test_rdd_fast_stark()
